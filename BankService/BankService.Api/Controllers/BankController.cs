848bb8565f6230b4048f152416c1364ab990c34a
Hemerson Leandro Siqueira Carlin <mersocarlin@gmail.com>
2016-03-28 12:51:14 -0700

    Add Api project

---
+++ BankService/BankService.Api/Controllers/BankController.cs
@@ -0,0 +1,31 @@
+﻿using BankService.Domain.Contracts;
+using BankService.Domain.Models;
+using Microsoft.AspNet.Mvc;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+namespace BankService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BankController : Controller
+    {
+        private readonly IAccountHolderRepository repository;
+
+        public BankController(IAccountHolderRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<AccountHolder> Get()
+        {
+            return this.repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public AccountHolder Get(string id)
+        {
+            return this.repository.GetById(new ObjectId(id));
+        }
+    }
+}
