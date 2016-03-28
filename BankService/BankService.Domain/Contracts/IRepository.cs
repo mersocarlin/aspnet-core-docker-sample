b0bd1d42e2b264771b6f5b0b938dead25e43280f
Hemerson Carlin <mersocarlin@gmail.com>
2016-03-28 16:42:41 -0700

    Add generic repository

---
+++ BankService/BankService.Domain/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BankService.Domain.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> GetWithLimit(int limit);
+        TEntity GetById(int id);
+        TEntity GetById(string id);
+        void Create(TEntity entity);
+        void Update(TEntity entity, int id);
+        void Update(TEntity entity, string id);
+        bool Delete(int id);
+        bool Delete(string id);
+    }
+}
