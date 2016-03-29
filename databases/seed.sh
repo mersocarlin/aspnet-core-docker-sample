57bad6614a5e17021b7170566a33ec8bc5df10c0
mersocarlin <mersocarlin@gmail.com>
2016-03-29 09:04:21 -0300

    Update seed script

--- databases/seed.sh
+++ databases/seed.sh
@@ -2,6 +2,6 @@
 
 mongoimport \
     --host 192.168.99.100 \
-    --jsonArray \
     --db bank \
-    --collection bank_data < ./databases/bank_data.json
+    --collection bank_data \
+    --file ./databases/bank_data.json
